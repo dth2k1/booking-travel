cf26927fda00f2df3e0d74458fb439c160763846
dth2k1 <gblhiep@gmail.com>
2023-02-26 01:29:51 +0700

    refs #1 tiny fix

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,8 @@
 class Room < ApplicationRecord
   belongs_to :user
 
-  has_many :calendars
-  has_many :photos
-  has_many :reservations
-  has_mnay :reviews
+  has_many :calendars, dependent: :destroy
+  has_many :photos, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+  has_mnay :reviews, dependent: :destroy
 end
