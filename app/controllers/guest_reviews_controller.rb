bc8951ad903d2516aa31de28cd237779bbea1016
dth2k1 <gblhiep@gmail.com>
2023-02-26 22:09:40 +0700

    refs #2 authen

---
+++ app/controllers/guest_reviews_controller.rb
@@ -0,0 +1,3 @@
+class GuestReviewsController < ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,67 @@ Rails.application.routes.draw do
   devise_for :user,
     :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
 
-  resources :rooms
-  get "search" => "pages#search"
+  resources :users, only: [:show] do
+    member do
+      post '/verify_phone_number' => 'users#verify_phone_number'
+      patch '/update_phone_number' => 'users#update_phone_number'
+    end
+  end
+
+  resources :rooms, except: [:edit] do
+    member do
+      get 'listing'
+      get 'pricing'
+      get 'description'
+      get 'photo_upload'
+      get 'amenities'
+      get 'location'
+      get 'preload'
+      get 'preview'
+    end
+    resources :photos, only: [:create, :destroy]
+    resources :reservations, only: [:create]
+    resources :calendars
+  end
+
+  resources :guest_reviews, only: [:create, :destroy]
+  resources :host_reviews, only: [:create, :destroy]
+
+  get '/your_trips' => 'reservations#your_trips'
+  get '/your_reservations' => 'reservations#your_reservations'
+
+  get 'search' => 'pages#search'
+
+  get 'dashboard' => 'dashboards#index'
+
+  resources :reservations, only: [:approve, :decline] do
+    member do
+      post '/approve' => "reservations#approve"
+      post '/decline' => "reservations#decline"
+    end
+  end
+
+  resources :revenues, only: [:index]
+
+  resources :conversations, only: [:index, :create] do
+    resources :messages, only: [:index, :create]
+  end
+
+  get '/host_calendar' => "calendars#host"
+
+  get '/payment_method' => "users#payment"
+  get '/payout_method' => "users#payout"
+  post '/add_card' => "users#add_card"
+
+  devise_scope :user do
+    get '/auth/stripe_connect/callback' => 'omniauth_callbacks#stripe_connect'
+    post '/auth/stripe_connect/callback' => 'omniauth_callbacks#stripe_connect'
+  end
+
+  get '/notification_settings' => "settings#edit"
+  post '/notification_settings' => "settings#update"
+
+  get '/notifications' => "notifications#index"
+
+  mount ActionCable.server => "/cable"
 end
